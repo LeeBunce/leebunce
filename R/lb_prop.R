c6fd24fdfbc49d39ba8f875b646aafff5f0b3c08
Lee Bunce <leebunce@hotmail.com>
2017-05-15 23:09:08 +0100

    Adds lb_prop function

---
+++ R/lb_prop.R
@@ -0,0 +1,20 @@
+#' Calculate a proportion from a vector
+#'
+#' @param x A vector to calculate a proportion from.
+#' @param numerator A vector of elements to count in the numerator.
+#' @param denominator A vector of elements to count in the denominator.
+#'
+#' @return The proportion of
+#'
+#' @example
+#' lb_prop(x = c(T, T, T, T, F, NA), numerator = T, denominator = c(T, F))
+
+lb_prop <- function(x, numerator, denominator){
+
+  n <- sum(x %in% numerator)
+  d <- sum(x %in% denominator)
+
+  prop <- n/d
+
+  prop
+}
