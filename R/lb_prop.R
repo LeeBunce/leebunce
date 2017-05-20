035fe047336fd9b0dc1c12c9a58762d185696411
Lee Bunce <leebunce@hotmail.com>
2017-05-20 14:36:37 +0100

    Change structure of numerator/denominator arguments. Now needs less typing.

--- R/lb_prop.R
+++ R/lb_prop.R
@@ -2,17 +2,17 @@
 #'
 #' @param x A vector to calculate a proportion from.
 #' @param numerator A vector of elements to count in the numerator.
-#' @param denominator A vector of elements to count in the denominator.
+#' @param denominator A vector of elements to count in the denominator in addition to those included in the numerator.
 #'
 #' @return The proportion of
 #'
 #' @example
-#' lb_prop(x = c(T, T, T, T, F, NA), numerator = T, denominator = c(T, F))
+#' lb_prop(x = c(T, T, T, T, F, NA), numerator = T, denominator = F)
 
 lb_prop <- function(x, numerator, denominator, percent = TRUE, digits = 1){
 
   n <- sum(x %in% numerator)
-  d <- sum(x %in% denominator)
+  d <- sum(x %in% c(numerator, denominator))
 
   prop <- n/d
 
