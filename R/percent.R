8fa257501c3b2f4117c7967b5db9cc61cf482615
Lee Bunce <leebunce@hotmail.com>
2017-06-24 08:29:41 +0100

    Adds percent function.

---
+++ R/percent.R
@@ -0,0 +1,15 @@
+#' Convert a proportion to a percentage.
+#'
+#' @param x A vaue to convert to a percentage.
+#' @param digits The number of decimal places to round to, passed to the round function.
+#'
+#' @return percent simply takes a proprtion and converts to a percentage by multiplying by 100 and rounding.
+#'
+#' @examples
+#' percent(0.43569)
+#' percent(0.43569, digits = 2)
+
+percent <- function(x, digits = 1){
+  percentage <- round(x*100, digits = digits)
+  percentage
+}
