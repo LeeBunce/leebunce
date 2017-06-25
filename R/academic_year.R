31f43cb9e49585bed61e755b1a83ba96ec9b200c
Lee Bunce <leebunce@hotmail.com>
2017-06-25 10:31:04 +0100

    Improve error handling of academic_year

--- R/academic_year.R
+++ R/academic_year.R
@@ -9,8 +9,7 @@
 #' academic_year(as.Date('2017-08-01'))
 
 academic_year <- function(x) {
-  if (lubridate::is.Date(x) == FALSE)
-    stop("x is not a date.", call. = FALSE)
+  if (any(!is.na(x) & !lubridate::is.Date(x))) stop("x is not a date.", call. = FALSE)
 
   academic_year <- ifelse(lubridate::month(x) < 8,
                           paste0(lubridate::year(x) - 1, '/', stringr::str_sub(lubridate::year(x), 3, 4)),
--- tests/testthat/test-academic_year.R
+++ tests/testthat/test-academic_year.R
@@ -5,3 +5,8 @@ test_that("Academic year gives the correct year", {
   expect_equal(academic_year(as.Date('2017-08-01')), '2017/18')
   expect_equal(academic_year(c(as.Date('2017-07-31'), as.Date('2017-08-01'))), c('2016/17', '2017/18'))
 })
+
+test_that("NA's are handled properly", {
+  expect_equal(academic_year(NA), NA)
+  expect_equal(academic_year(c(as.Date('2017-07-31'), NA)), c('2016/17', NA))
+})
