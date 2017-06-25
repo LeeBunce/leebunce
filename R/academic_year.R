08b604a74d7995766cbc20572d809d80941ef16f
Lee Bunce <leebunce@hotmail.com>
2017-06-25 09:10:22 +0100

    Update dependencies (stringr)

--- R/academic_year.R
+++ R/academic_year.R
@@ -12,9 +12,9 @@ academic_year <- function(x) {
   if (lubridate::is.Date(x) == FALSE)
     stop("x is not a date.", call. = FALSE)
 
-  academic_year <- ifelse(month(x) < 8,
-                          paste0(year(x) - 1, '/', str_sub(year(x), 3, 4)),
-                          paste0(year(x), '/', str_sub(year(x) + 1, 3, 4)))
+  academic_year <- ifelse(lubridate::month(x) < 8,
+                          paste0(year(x) - 1, '/', stringr::str_sub(year(x), 3, 4)),
+                          paste0(year(x), '/', stringr::str_sub(year(x) + 1, 3, 4)))
 
   academic_year
 }
