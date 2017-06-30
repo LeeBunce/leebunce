fb4e21feae5ef4c9a87d4da26b870ab2f261b9dc
Lee Bunce <leebunce@hotmail.com>
2017-06-30 18:54:44 +0100

    Additional test for clean_uun

--- tests/testthat/test-clean_uun.R
+++ tests/testthat/test-clean_uun.R
@@ -2,5 +2,6 @@ context("clean_uun")
 
 test_that("clean_uun gives the right value.", {
   expect_equal(clean_uun('s1234567'), 'S1234567')
+  expect_equal(clean_uun(c('s1234567', 's1234567')), c('S1234567', 'S1234567'))
   expect_equal(clean_uun(NA), NA)
 })
