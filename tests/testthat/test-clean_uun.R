fc3484c28bbd124381020a1dbf2ad0cb49694cd6
Lee Bunce <leebunce@hotmail.com>
2017-10-12 23:03:20 +0100

    add test for issue #10

--- tests/testthat/test-clean_uun.R
+++ tests/testthat/test-clean_uun.R
@@ -6,4 +6,5 @@ test_that("clean_uun gives the right value.", {
   expect_equal(clean_uun(c('s1234567', 's1234567')), c('S1234567', 'S1234567'))
   expect_equal(clean_uun(NA), NA)
   expect_warning(clean_uun('a string'))
+  expect_silent(clean_uun('S1234567'))
 })
