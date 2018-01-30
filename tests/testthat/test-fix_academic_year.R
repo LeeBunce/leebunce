2f09d1a5f82d2e08482fc46db9449ab7e8334f60
Lee Bunce <leebunce@hotmail.com>
2018-01-30 22:46:06 +0000

    extra test for fix_academic_year

--- tests/testthat/test-fix_academic_year.R
+++ tests/testthat/test-fix_academic_year.R
@@ -1,11 +1,12 @@
 context("fix_academic_year")
 
-test_that("fix_academic__year gives the correct value", {
+test_that("fix_academic_year gives the correct value", {
   expect_equal(fix_academic_year('2014/5'), '2014/15')
   expect_equal(fix_academic_year(c('2014/5', '2015/6')), c('2014/15', '2015/16'))
+  expect_error(fix_academic_year('2014/65'))
 })
 
-test_that("fix_academic__year handles NA correctly", {
+test_that("fix_academic_year handles NA correctly", {
   expect_equal(is.na(fix_academic_year(NA)), TRUE)
   expect_equal(fix_academic_year(c('2014/5', NA)), c('2014/15', NA))
 })
