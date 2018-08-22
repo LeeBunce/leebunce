171509dafdbb3f8d67e5a209e1343d2cffc0a757
Lee Bunce <lbunce@ed.ac.uk>
2018-08-22 16:23:07 +0100

    Add widgets to project template

--- R/leebunce_project_template.R
+++ R/leebunce_project_template.R
@@ -1,18 +1,23 @@
 # Project template function
 
-leebunce_project_template <- function(path){
+leebunce_project_template <- function(path, data, scripts, output){
   dir.create(path, recursive = TRUE)
 
-  dir.create(paste0(path, '/Data'))
-  dir.create(paste0(path, '/Scripts'))
-  dir.create(paste0(path, '/Output'))
+  if(data == TRUE) dir.create(paste0(path, '/Data'))
 
-  fileConn<-file(paste0(path, '/Scripts/Main.R'))
-  writeLines(c("# Title",
-               "",
-               "# Packages ----------------------------------------------------------------",
-               "library(tidyverse)",
-               "library(leebunce)"), fileConn)
-  close(fileConn)
+  if(scripts == TRUE) {
+    dir.create(paste0(path, '/Scripts'))
+
+    fileConn<-file(paste0(path, '/Scripts/Main.R'))
+    writeLines(c("# Title",
+                 "",
+                 "# Packages ----------------------------------------------------------------",
+                 "library(tidyverse)",
+                 "library(leebunce)"), fileConn)
+    close(fileConn)
+
+    }
+
+  if(output == TRUE) dir.create(paste0(path, '/Output'))
 
 }
