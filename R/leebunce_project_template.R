ee6a069e38c6bf26209c20db4037fc6b23a47c59
Lee Bunce <lbunce@ed.ac.uk>
2018-08-23 11:35:29 +0100

    Remove unnecessary code

--- R/leebunce_project_template.R
+++ R/leebunce_project_template.R
@@ -3,9 +3,9 @@
 leebunce_project_template <- function(path, data, scripts, output){
   dir.create(path, recursive = TRUE)
 
-  if(data == TRUE) dir.create(paste0(path, '/Data'))
+  if(data) dir.create(paste0(path, '/Data'))
 
-  if(scripts == TRUE) {
+  if(scripts) {
     dir.create(paste0(path, '/Scripts'))
 
     file.copy(from = system.file('misc', 'Main.R', package = 'leebunce'),
@@ -13,6 +13,6 @@ leebunce_project_template <- function(path, data, scripts, output){
 
     }
 
-  if(output == TRUE) dir.create(paste0(path, '/Output'))
+  if(output) dir.create(paste0(path, '/Output'))
 
 }
