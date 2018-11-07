4378d00bc16e143c66bc2e39cbfb2795c267e8f8
leebunce <leebunce@hotmail.com>
2018-11-07 22:16:54 +0000

    Make round_n a generic and add method for data.frames

--- R/round_n.R
+++ R/round_n.R
@@ -1,21 +1,28 @@
 #' Round to a specified integer
 #'
-#' This function rounds a number to a specified integer.
+#' This function rounds a number to a specified integer. If x is a data.frame then rounding is applied to all numeric variables.
 #'
-#' @param x A numeric vector.
+#' @param x A numeric vector or a data.frame.
 #'
 #' @param n An integer to round to.
 #'
-#' @return A numeric vector.
+#' @return A numeric vector or a data.frame.
 #'
 #' @examples
 #' round_n(16, 5)
 #' round_n(c(7, 16, NA), 5)
+#' round_n(iris, 5)
 #' @export
 
-round_n <- function(x, n) {
+round_n <- function(x, n) UseMethod("round_n")
+
+round_n.numeric <- function(x, n) {
   if (!(n - round(n) == 0) | n <= 0) stop('n must be a positive whole number')
 
   n*round(x/n)
 
+}
+
+round_n.data.frame <- function(x, n){
+  dplyr::mutate_if(x, is.numeric, round_n.numeric, n = n)
 }
--- tests/testthat/test-round_n.R
+++ tests/testthat/test-round_n.R
@@ -3,8 +3,13 @@ context("round_n")
 test_that("round_n gives the right value.", {
   expect_equal(round_n(16, 5), 15)
   expect_equal(round_n(c(7, 16, NA), 5), c(5, 15, NA))
-  expect_equal(is.na(round_n(NA, 5)), TRUE)
+  expect_error(is.na(round_n(NA, 5)))
   expect_error(round_n(16, 5.5))
   expect_error(round_n(16, 0))
   expect_error(round_n(16, -5))
+})
+
+test_that("round_n handles data frames.", {
+  expect_equal(class(round_n(iris, 5)),  'data.frame')
+  expect_equal(round_n(iris, 5)[1, 1], 5)
 })
