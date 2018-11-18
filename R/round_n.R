50dd5c29546bc97c5b2f17699eead9c8bde6a5a0
leebunce <leebunce@hotmail.com>
2018-11-18 12:33:54 +0000

    Typo

--- R/round_n.R
+++ R/round_n.R
@@ -1,7 +1,7 @@
 #' Round to a specified integer
 #'
 #' This function rounds a number to a specified integer. If x is a data.frame then rounding is applied to all numeric variables.
-#' The function round_5 gives a particularly common use case, as published data is often rounded to the nearest 5 for disclodure control.
+#' The function round_5 gives a particularly common use case, as published data is often rounded to the nearest 5 for disclosure control.
 #'
 #' @param x A numeric vector or a data.frame.
 #'
